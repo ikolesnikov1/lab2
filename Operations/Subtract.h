2b1db4885fe11994e51270c7d6c11323f3023416
llya <70789333+ikolesnikov1@users.noreply.github.com>
2020-12-20 09:52:42 +0300

    Delete Subtract.h

--- Operations/Subtract.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "../Interfaces/IOperation.h"
-
-class Subtract : public IOperation {
-public:
-    void execute(std::list<std::string>& args, Context & ctx) const override;
-};
