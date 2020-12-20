57c56bd3a1cee41980271f4414d984212c294505
llya <70789333+ikolesnikov1@users.noreply.github.com>
2020-12-20 09:47:47 +0300

    Delete Reader.h

--- Reader.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-class Reader {
-public:
-    static void splitString(std::list <std::string>&, const std::string&);
-    static bool readNextString(std::istream& is, std::list <std::string>& args);
-};
