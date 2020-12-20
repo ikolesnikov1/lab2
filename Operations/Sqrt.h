b102965323bd3182ce68b5f23575e9841678dec8
llya <70789333+ikolesnikov1@users.noreply.github.com>
2020-12-20 09:52:29 +0300

    Delete Sqrt.h

--- Operations/Sqrt.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "../Interfaces/IOperation.h"
-#include <cmath>
-
-class Sqrt : public IOperation {
-public:
-    void execute(std::list<std::string>& args, Context & ctx) const override;
-};
