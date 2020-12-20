f7e5dfa696fd4b8d4fefb018340c8f2735411f47
llya <70789333+ikolesnikov1@users.noreply.github.com>
2020-12-20 09:49:14 +0300

    Delete NoSuchOperation.h

--- Exceptions/NoSuchOperation.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "EvaluatorException.h"
-
-class NoSuchOperation : public EvaluatorException {
-public:
-    NoSuchOperation() : EvaluatorException("No such operation!"){};
-};
