7f59be143dcc0b40afecffcfabd65d5a2f621236
llya <70789333+ikolesnikov1@users.noreply.github.com>
2020-12-20 09:49:32 +0300

    Delete WrongIdentifier.h

--- Exceptions/WrongIdentifier.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "EvaluatorException.h"
-
-class WrongIdentifier : public EvaluatorException {
-public:
-    WrongIdentifier() : EvaluatorException("Wrong identifier format!"){};
-};
