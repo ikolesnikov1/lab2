69b5a8b5a7e9c34de427cff27e27905198b131b7
llya <70789333+ikolesnikov1@users.noreply.github.com>
2020-12-20 09:49:20 +0300

    Delete OutOfParameters.h

--- Exceptions/OutOfParameters.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "EvaluatorException.h"
-
-class OutOfParameters : public EvaluatorException {
-public:
-    OutOfParameters() : EvaluatorException("Out of parameters!"){};
-};
