01419461e691feda0325f44251a840881f020a19
llya <70789333+ikolesnikov1@users.noreply.github.com>
2020-12-20 09:49:02 +0300

    Delete NegativeParameter.h

--- Exceptions/NegativeParameter.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "EvaluatorException.h"
-
-class NegativeParameter : public EvaluatorException {
-public:
-    NegativeParameter() : EvaluatorException("Negative parameter!"){};
-};
