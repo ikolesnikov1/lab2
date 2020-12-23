abe998d958fc3a632facfdb4f925271fa222fad5
llya <70789333+ikolesnikov1@users.noreply.github.com>
2020-12-23 17:35:22 +0300

    Add files via upload

--- Operations/Push.cpp
+++ Operations/Push.cpp
@@ -7,23 +7,14 @@ void Push::execute(std::list<std::string> &args, Context &ctx) const {
     if (args.empty()) {
         throw WrongInput();
     }
-
     std::string number = args.front();
-    bool isNumber = false;
-    for (char i : number) {
-        if (isdigit(i)) {
-            isNumber = true;
-            break;
-        }
-    }
 
-    if (isNumber) {
-        try {
-            ctx.operands.push(stof(number));
-        } catch (std::invalid_argument &ex) {
-            throw WrongInput();
-        }
-    } else if (ctx.defines.find(number) != ctx.defines.end()) {
+    try {
+        ctx.operands.push(stof(number));
+    } catch (std::invalid_argument &ex) {
+        if (ctx.defines.find(number) != ctx.defines.end())
             ctx.operands.push(ctx.defines[number]);
+        else
+            throw WrongNumber();
     }
 }
